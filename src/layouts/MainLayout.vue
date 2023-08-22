fdcea535c1fdb2ca355ce2af955d4a44bd4095ab
Abel Alazar Dirar <aadlinux358@gmail.com>
2023-08-22 16:16:41 +0300

    frontend prototype

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,40 +2,32 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+        <q-btn flat
+               dense
+               round
+               icon="menu"
+               aria-label="Menu"
+               @click="toggleLeftDrawer" />
 
         <q-toolbar-title>
-          Quasar App
+          My Own Voice
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div>Logout</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen"
+              show-if-above
+              bordered>
       <q-list>
-        <q-item-label
-          header
-        >
+        <q-item-label header>
           Essential Links
         </q-item-label>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+        <EssentialLink v-for="link in essentialLinks"
+                       :key="link.title"
+                       v-bind="link" />
       </q-list>
     </q-drawer>
 
@@ -46,8 +38,8 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue';
+import {ref} from 'vue';
+import EssentialLink, {EssentialLinkProps} from 'components/EssentialLink.vue';
 
 const essentialLinks: EssentialLinkProps[] = [
   {
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,42 +1,148 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page padding>
+    <div class="column justify-center items-center q-my-md">
+      <div class="text-h4">
+        Voice Diary
+      </div>
+    </div>
+
+    <div class="row justify-center items-center q-my-md">
+      <q-btn :color="recording ? 'red' : 'primary'"
+             round
+             size="xl"
+             :icon="recording ? 'mic_off' : 'mic'"
+             @click="toggleRecording"></q-btn>
+    </div>
+    <div class="row justify-center items-center q-my-md">
+      <div v-if="selectedAudio?.audioUrl">
+        Playing: {{ selectedAudio?.fileName }}
+      </div>
+      <audio controls
+             autoplay
+             :src="selectedAudio?.audioUrl"></audio>
+    </div>
+    <div class="row justify-center items-center q-my-md">
+      {{ audios.size === 0 ? 'No diaries' : 'Audio diaries' }}
+    </div>
+    <q-list>
+      <q-item v-ripple
+              v-for="[k, v] of audios"
+              :key="k">
+        <q-item-section avatar>
+
+          <q-icon color="primary"
+                  name="audiotrack" />
+        </q-item-section>
+        <q-item-section>{{ v.fileName }}</q-item-section>
+        <q-item-section side>
+          <div class="row q-gutter-sm">
+
+            <q-btn icon="play_arrow"
+                   round
+                   size="sm"
+                   @click="playAudio(v)"
+                   color="primary" />
+            <q-icon size="md"
+                    :color="v.isFavorite ? 'red' : 'grey'"
+                    :name="v.isFavorite ? 'favorite' : 'favorite_border'"
+                    @click="v.isFavorite = !v.isFavorite" />
+            <!--
+            <q-btn icon="favorite_border"
+                   round
+                   size="sm"
+                   :color="v.isFavorite ? 'red' : 'grey'"
+                   @click="v.isFavorite = !v.isFavorite" />
+              -->
+
+            <q-btn icon="delete"
+                   round
+                   size="sm"
+                   @click="deleteAudio(k)"
+                   color="red" />
+          </div>
+        </q-item-section>
+      </q-item>
+    </q-list>
+    <div class="text-h5">
+      Favorites
+    </div>
+    <div v-for="[k, v] of audios"
+         :key="k">
+      <div v-if="v.isFavorite">
+        {{ v.fileName }} is favorite: {{ v.isFavorite }}
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { ref } from 'vue';
-
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
+import {ref, onMounted, Ref} from 'vue';
+export interface AudioFile {
+  fileName: string;
+  audioUrl: string;
+  isFavorite: boolean
+}
+const mediaRecorder: Ref<MediaRecorder | null> = ref(null)
+const chunks: Ref<BlobPart[]> = ref([])
+const audios: Ref<Map<string, AudioFile>> = ref(new Map())
+const recording = ref(false)
+const selectedAudio: Ref<AudioFile | null> = ref(null)
+function startRecording() {
+  if (mediaRecorder.value) {
+    mediaRecorder.value.start()
+    console.log(mediaRecorder.value.state)
+    console.log('recorder started')
+    recording.value = true
+  } else {
+    alert('Can not start recording.')
   }
-]);
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+}
+
+function stopRecording() {
+  if (mediaRecorder.value) {
+    mediaRecorder.value.stop()
+    console.log(mediaRecorder.value.state)
+    console.log('recorder stoped')
+    recording.value = false
+  }
+}
+function toggleRecording() {
+  if (recording.value) {
+    stopRecording()
+  } else {
+    startRecording()
+  }
+}
+
+function playAudio(audio: AudioFile) {
+  selectedAudio.value = audio
+}
+
+function deleteAudio(key: string) {
+  selectedAudio.value = null
+  audios.value.delete(key)
+}
+
+onMounted(() => {
+  if (navigator.mediaDevices?.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({audio: true})
+      .then((stream) => {
+        mediaRecorder.value = new MediaRecorder(stream)
+        mediaRecorder.value.ondataavailable = (e: BlobEvent) => {
+          chunks.value.push(e.data)
+        }
+        mediaRecorder.value.onstop = (e) => {
+          console.log("Data available after MediaRecorder.stop() called.")
+          const audioName = prompt('Enter audio file name: ', `Unnamed audio ${new Date().toString()}`)
+          const blob = new Blob(chunks.value, {type: 'audio/ogg; codecs=opus'})
+          const audioURL = window.URL.createObjectURL(blob)
+          chunks.value = []
+          audios.value.set(audioName!, {fileName: audioName!, audioUrl: audioURL, isFavorite: false})
+        }
+      }).catch((err) => console.log('The following getUserMedia error occured: ' + err))
+  } else {
+    alert('getUserMedia not supported on your browser!')
+  }
+
+})
 </script>
